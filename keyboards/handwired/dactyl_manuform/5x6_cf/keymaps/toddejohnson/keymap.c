9070d50797233c6b6298609f8910b4ef44d6d537
Todd E Johnson <todd@toddejohnson.net>
2025-01-06 15:44:27 -0600

    Swap keymap

--- keyboards/handwired/dactyl_manuform/5x6_cf/keymaps/toddejohnson/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_cf/keymaps/toddejohnson/keymap.c
@@ -19,8 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                           KC_LBRC, KC_RBRC,                                          KC_MINS, KC_EQL,
-                                            LOWER,   KC_LALT, KC_BSPC,KC_SPC, KC_ENT,RAISE,
-                                            KC_LGUI, KC_LCTL,KC_SPC,  KC_DEL, KC_HOME,KC_END
+                                            LOWER,   KC_BSPC,KC_LALT, KC_ENT, KC_SPC, RAISE,
+                                            KC_LGUI, KC_SPC, KC_LCTL, KC_DEL, KC_HOME,KC_END
     ),
 
     [_LOWER] = LAYOUT(
