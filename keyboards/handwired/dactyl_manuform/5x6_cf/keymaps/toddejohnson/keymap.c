58026b0e384a0a33e0edf1fba92b3893e8d3728a
Todd E Johnson <todd@toddejohnson.net>
2025-01-09 15:22:41 -0600

    Swap keymap

--- keyboards/handwired/dactyl_manuform/5x6_cf/keymaps/toddejohnson/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_cf/keymaps/toddejohnson/keymap.c
@@ -14,8 +14,8 @@ enum custom_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,  KC_8,    KC_9,   KC_0,    KC_BSLS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,    KC_PGUP,
+        QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,  KC_8,    KC_9,   KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,    KC_BSLS,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                           KC_LBRC, KC_RBRC,                                          KC_MINS, KC_EQL,
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT(
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         _______, _______, _______, _______, _______, KC_LBRC,        KC_RBRC, KC_P7,   KC_P8,   KC_P9,   _______, _______,
         _______, KC_SCRL, KC_INS,  KC_PAUS, KC_PSCR, KC_LPRN,        KC_RPRN, KC_P4,   KC_P5,   KC_P6,   _______, _______,
         _______, _______, _______, _______, _______, _______,        _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT(
         KC_F12,  KC_F1,   KC_F2,   KC_F3 ,  KC_F4,   KC_F5,          KC_F6,   KC_F7 ,  KC_F8,   KC_F9,   KC_F10 , KC_F11,
-        _______, _______, _______, _______, _______, KC_TRNS,        KC_TRNS, _______, KC_NUM,  _______, _______, KC_MUTE,
-        _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_TRNS,        KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_VOLU,
+        _______, _______, _______, _______, _______, KC_TRNS,        KC_TRNS, _______, KC_INS,  _______, KC_PSCR, KC_MUTE,
+        _______, _______, _______, KC_DEL, _______, KC_TRNS,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_VOLU,
         _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, KC_VOLD,
                           _______, _______,                                            _______, _______,
                                             _______, _______,_______, _______, _______, _______,
