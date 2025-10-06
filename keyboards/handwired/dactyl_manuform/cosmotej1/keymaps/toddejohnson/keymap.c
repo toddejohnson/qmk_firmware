c15c8d7d76b687244f4f997fc086c6d7a4c87dec
Todd E Johnson <todd@toddejohnson.net>
2025-10-06 18:01:26 -0500

    Assorted changes cosmos add default

--- keyboards/handwired/dactyl_manuform/cosmotej1/keymaps/toddejohnson/keymap.c
+++ keyboards/handwired/dactyl_manuform/cosmotej1/keymaps/toddejohnson/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                                         KC_LCTL, KC_SPC,  LOWER,   RAISE,   KC_SPC, KC_ENT,
-                                        KC_LALT, KC_BSPC, KC_LBRC, KC_RBRC, KC_DEL, KC_LGUI
+                                        KC_LALT, KC_BSPC, KC_LSFT, KC_RSFT, KC_DEL, KC_LGUI
     ),
 
     [_RAISE] = LAYOUT(
